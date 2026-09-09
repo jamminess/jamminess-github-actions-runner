3270d7a84fff6dc81a8aa2f7f53da6b0dd658a45
s99mj <sandhya.gandalwar@justice.gov.uk>
2026-09-09 13:45:22 +0100

    Replace legacy apt-key with  signed-by keyring

--- build/sources.sh
+++ build/sources.sh
@@ -5,13 +5,24 @@ function configure_git() {
   # shellcheck source=/dev/null
   source /etc/os-release
 
-  local GIT_CORE_PPA_KEY="A1715D88E1DF1F24"
-  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ${GIT_CORE_PPA_KEY} \
-    || apt-key adv --keyserver pgp.mit.edu --recv-keys ${GIT_CORE_PPA_KEY} \
-    || apt-key adv --keyserver keyserver.pgp.com --recv-keys ${GIT_CORE_PPA_KEY}
-
   if [[ "${VERSION_CODENAME}" == "focal" ]]; then
-    echo deb http://ppa.launchpad.net/git-core/ppa/ubuntu focal main>/etc/apt/sources.list.d/git-core.list
+    mkdir -p /etc/apt/keyrings
+
+    local GIT_CORE_PPA_KEY="A1715D88E1DF1F24"
+    local GNUPGHOME
+    GNUPGHOME=$(mktemp -d)
+    chmod 700 "${GNUPGHOME}"
+
+    gpg --batch --homedir "${GNUPGHOME}" --keyserver keyserver.ubuntu.com --recv-keys ${GIT_CORE_PPA_KEY} \
+      || gpg --batch --homedir "${GNUPGHOME}" --keyserver pgp.mit.edu --recv-keys ${GIT_CORE_PPA_KEY} \
+      || gpg --batch --homedir "${GNUPGHOME}" --keyserver keyserver.pgp.com --recv-keys ${GIT_CORE_PPA_KEY}
+
+    gpg --batch --homedir "${GNUPGHOME}" --export ${GIT_CORE_PPA_KEY} \
+      | gpg --dearmor -o /etc/apt/keyrings/git-core-ppa.gpg
+    rm -rf "${GNUPGHOME}"
+
+    echo "deb [signed-by=/etc/apt/keyrings/git-core-ppa.gpg] http://ppa.launchpad.net/git-core/ppa/ubuntu focal main" \
+      > /etc/apt/sources.list.d/git-core.list
   fi
 }
 
@@ -38,11 +49,13 @@ function configure_container_tools() {
   source /etc/os-release
 
   if [[ "${VERSION_CODENAME}" == "focal" ]]; then
+    mkdir -p /etc/apt/keyrings
+
     echo "available in 20.10 and higher"
-    echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_20.04/ /" \
+    echo "deb [signed-by=/etc/apt/keyrings/libcontainers-stable.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_20.04/ /" \
       | tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
     curl -L "https://build.opensuse.org/projects/devel:kubic/signing_keys/download?kind=gpg" \
-      | apt-key add -
+      | gpg --dearmor -o /etc/apt/keyrings/libcontainers-stable.gpg
   fi
 }
 
@@ -57,4 +70,7 @@ function remove_sources() {
   rm -f /etc/apt/sources.list.d/git-core.list
   rm -f /etc/apt/sources.list.d/docker.list
   rm -f /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+  rm -f /etc/apt/keyrings/git-core-ppa.gpg
+  rm -f /etc/apt/keyrings/docker.gpg
+  rm -f /etc/apt/keyrings/libcontainers-stable.gpg
 }
